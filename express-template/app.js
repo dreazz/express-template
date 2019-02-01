edfcf159499937d748c2940d4fb0a82dd565ef31
dreazz <agustinsilveira.96@gmail.com>
2019-02-01 16:05:22 +0100

    partials and layout create

--- express-template/app.js
+++ express-template/app.js
@@ -13,6 +13,7 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layouts/layout');
 app.set('view engine', 'ejs');
 app.set('layout extractScripts', true);
 
